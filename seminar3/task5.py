ef9854dd0cdf5a72c8d50e9f95c985926bf2c35c
Elyor <e.hadjihanov@gmail.com>
2020-10-15 10:48:49 +0500

    added seminar 2 task 5

---
+++ seminar3/task5.py
@@ -0,0 +1,8 @@
+def print_num_in_different_systems(num):
+    print("Num = ", num)
+    print("Binary = ", f'{num:b}')
+    print("Octal = ", f'{num:o}')
+    print("Hexadecimal = ", f'{num:x}'.upper())
+
+
+print_num_in_different_systems(246)
